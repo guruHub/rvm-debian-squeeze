db41f1268419e73e7e418d6bfdd190dc5db46e99
Martin Loy <martinloy.uy@gmail.com>
2012-08-07 14:08:32 -0300

    Update ruby1.9.2-rvm-ready-debian-squeeze.sh
    rvm requirements added

--- ruby1.9.2-rvm-ready-debian-squeeze.sh
+++ ruby1.9.2-rvm-ready-debian-squeeze.sh
@@ -2,8 +2,9 @@
 
 sudo apt-get update
 
-sudo apt-get -y -V install autoconf automake autotools-dev build-essential bison bzip2 curl git libreadline5 libsqlite3-0 sqlite3 libsqlite3-dev libxml2-dev libmysqlclient-dev libreadline5-dev libruby openssl libssl-dev zlib1g zlib1g-dev zlibc git-core gcc make libxml2-dev libxslt-dev libopenssl-ruby libncurses5-dev libapr1-dev libaprutil1-dev build-essential libcurl4-openssl-dev
-  
+#sudo apt-get -y -V install autoconf automake autotools-dev build-essential bison bzip2 curl git libreadline5 libsqlite3-0 sqlite3 libsqlite3-dev libxml2-dev libmysqlclient-dev libreadline5-dev libruby openssl libssl-dev zlib1g zlib1g-dev zlibc git-core gcc make libxml2-dev libxslt-dev libopenssl-ruby libncurses5-dev libapr1-dev libaprutil1-dev build-essential libcurl4-openssl-dev
+sudo apt-get -y -V install autoconf automake autotools-dev bison build-essential bzip2 curl gcc git git-core libapr1-dev libaprutil1-dev libc6-dev libcurl4-openssl-dev libmysqlclient-dev libncurses5-dev libopenssl-ruby libreadline5 libreadline5-dev libreadline6 libreadline6-dev libruby libsqlite3-0 libsqlite3-dev libssl-dev libtool libxml2-dev libxslt-dev libyaml-dev make ncurses-dev openssl sqlite3 subversion zlib1g zlib1g-dev zlibc
+
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> .bashrc
